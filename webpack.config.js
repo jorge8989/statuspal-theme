2a1dd2317da078c925103c8fe212d2c20c47598f
Eduardo Messuti <messuti.edd@gmail.com>
2018-09-11 22:07:55 +0200

    parsing basic status page

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,43 @@
 const path = require('path');
+const fetch = require('node-fetch');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
-module.exports = {
-  mode: mode,
-  entry: './src/js/index.js',
-  output: {
-    filename: 'js/[hash].bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.liquid$/, use: [
-          { loader: "html-loader" },
-          { loader: "liquid-loader", options: { data: { var1: 'YOLO' } } }
-        ]
-      },
-      { test: /\.scss$/, use: ExtractTextPlugin.extract({fallback: 'style-loader', use: ['css-loader', 'sass-loader']}) },
-      { test: /\.css$/, use: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader'}) },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: __dirname + '/src/templates/index.liquid' }),
-    new ExtractTextPlugin('css/[hash].[name].css'),
-  ]
+const getConfig = (data) => {
+  const htmlLoaders = [{ loader: "html-loader" }];
+  if (mode !== 'production') htmlLoaders.push({
+    loader: "liquid-loader", options: { data, context: path.resolve(__dirname, 'src/templates'), extension: 'liquid' }
+  });
+  return {
+    mode: mode,
+    entry: './src/js/app.js',
+    output: {
+      filename: 'js/[name]-[hash].js',
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        { test: /\.liquid|\.html$/, use: htmlLoaders },
+        { test: /\.scss$/, use: ExtractTextPlugin.extract({fallback: 'style-loader', use: ['css-loader', 'sass-loader']}) },
+        { test: /\.css$/, use: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader'}) },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: __dirname + '/src/templates/index.liquid' }),
+      new ExtractTextPlugin('css/[name]-[hash].css'),
+      new CleanWebpackPlugin([path.resolve(__dirname, 'dist/*')]),
+    ]
+  };
+};
+
+module.exports = () => {
+  return new Promise((resolve, reject) => {
+    fetch('http://localhost:4000/api/v1/status_pages/meta')
+      .then(res => res.json())
+      .then(json => resolve(getConfig(json)))
+      .catch(e => reject(e));
+  });
 };
