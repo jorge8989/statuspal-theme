d08c6fa281c4e336c4c2b91f00af8e722e80938d
jorge8989 <jorgearteaga8936@gmail.com>
2019-07-12 14:13:31 -0400

    added autorefresh script

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,7 @@ window.setLocale = function (el) {
     console.log(el.value);
     window.location.href = window.location.href.split("?")[0] + "?lang=" + el.value;
 }
+
+var autoRefreshInterval = setInterval(function() {
+  location.reload();
+}, 60000);
